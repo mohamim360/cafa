4f0e5094e3b210073611819ba4bec78ba6ee6fa4
mohamim360 <mohamim360@gmail.com>
2023-04-01 12:50:19 +0600

    added nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './Header.css';
 const Header = () => {
   return (
-    <div>
-      <h1>Knowledge Cafe</h1>
-    </div>
+    <nav className='header'>
+      <h2>Knowledge Cafe</h2>
+      <div>
+      <a href="/home">home</a>
+      <a href="/bookmark">bookmark</a>
+      <a href="/blog">blog</a>
+      </div>
+     
+      
+    </nav>
+ 
   );
 };
 
