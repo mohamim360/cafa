dc6cebdc921639e38e923597df84e92ced425d28
mohamim360 <mohamim360@gmail.com>
2023-04-01 14:08:28 +0600

    created fake data

---
+++ src/components/Blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Blog.css'
+const Blog = () => {
+  return (
+    <div className='blog-container'>
+      <div className='blog'>
+<h1>dddd</h1>
+      </div>
+
+      <div className='bookmark'>
+<h1>ddd</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
